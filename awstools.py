6aa66f346fa2463f56eb20647646b028be6c47e1
Toomore Chiang <toomore0929@gmail.com>
2013-12-15 19:06:59 +0800

    PEP8 ./awstools.py

--- awstools.py
+++ awstools.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+''' My AWS Tools '''
 from boto import ec2
 from boto.ec2.blockdevicemapping import BlockDeviceMapping
 from boto.ec2.blockdevicemapping import BlockDeviceType
@@ -6,27 +7,37 @@ import setting
 
 
 class AwsTools(object):
+    ''' AWS tools '''
 
     def __init__(self):
+        ''' Make a connect '''
         self.conn = ec2.connect_to_region(setting.REGION,
                                      aws_access_key_id=setting.ID,
                                      aws_secret_access_key=setting.KEY)
 
     def get_all_instances(self):
+        ''' Get all on running instances'''
         return self.conn.get_all_instances(filters={'instance-state-code': 16})
 
     def create_snapshot(self, dry_run=False):
+        '''Create a snapshot with running instances '''
         for i in self.get_all_instances():
             print 'instance', i.instances[0].id
-            volume = self.conn.get_all_volumes(filters={'attachment.instance-id': i.instances[0].id})
-            print 'volume', volume
+            volumes = self.conn.get_all_volumes(filters={
+                    'attachment.instance-id': i.instances[0].id})
+            print 'volume', volumes
 
-            for v in volume:
-                snap = v.create_snapshot('From boto.', dry_run)
+            for volume in volumes:
+                snap = volume.create_snapshot('From boto.', dry_run)
                 print 'create_snapshot', snap
-                print 'Add tag', self.conn.create_tags(snap.id, {'Name': 'AUTO-SNAP','CreatedBy': 'boto'}, dry_run)
+                print 'Add tag', self.conn.create_tags(snap.id,
+                        {'Name': 'AUTO-SNAP','CreatedBy': 'boto'}, dry_run)
 
-    def register_image(self, snapshot_id, root_device_name, delete_on_termination=False):
+    def register_image(self, snapshot_id, root_device_name,
+            delete_on_termination=False):
+        ''' Register a AMI '''
+
+        # To make delete_on_termination, need to create a block_device_map
         root_vol = BlockDeviceType(snapshot_id=snapshot_id,
                                    delete_on_termination=delete_on_termination)
         block_device_map = BlockDeviceMapping()
@@ -40,5 +51,6 @@ class AwsTools(object):
                                         block_device_map=block_device_map)
 
 if __name__ == '__main__':
+    print 'remove comment before use'
     #AwsTools().create_snapshot()
-    print AwsTools().register_image('snap-5e6abe7a', '/dev/sda1', True)
+    #print AwsTools().register_image('snap-c7e9e228', '/dev/sda1', True)
