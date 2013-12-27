741a953dc27765fc09886baa16f0035a7621f67f
Toomore Chiang <toomore0929@gmail.com>
2013-12-27 10:02:44 +0000

    Add result return snap.

--- awsec2tools.py
+++ awsec2tools.py
@@ -35,6 +35,7 @@ class AwsEC2Tools(object):
 
         print instances_id
 
+        result_snap = []
         for i in instances_id:
             volumes = self.conn.get_all_volumes(filters={
                     'attachment.instance-id': i})
@@ -43,10 +44,13 @@ class AwsEC2Tools(object):
             for volume in volumes:
                 now = datetime.now()
                 snap = volume.create_snapshot('%s %s' % (i, now), dry_run)
+                result_snap.append(snap)
                 print 'create_snapshot', snap
                 print 'Add tag', self.conn.create_tags(snap.id,
                         {'Name': 'AUTO-%s%s-%s' % (now.month, now.day, i),
                          'CreatedBy': 'boto'}, dry_run)
+
+        return result_snap
 
     def register_image(self, snapshot_id, root_device_name,
             delete_on_termination=False):
