58c08de76dfe254da2dedb99e075438559eb6b19
Toomore Chiang <toomore0929@gmail.com>
2013-12-31 09:27:02 +0000

    Add snapshot and regist AMIs.

--- scripts/daily_snapshot.py
+++ scripts/daily_snapshot.py
@@ -2,9 +2,13 @@
 import sys
 sys.path.append('../')
 
-from awsec2tools import AwsEC2Tools
 from awsec2tools import AwsEC2MetaData
+from awsec2tools import AwsEC2Tools
+import time
 
 # Deploy to an instance and using cron.
 INSTANCE_ID = AwsEC2MetaData().get('instance-id')
-print AwsEC2Tools().create_snapshot([INSTANCE_ID, ])
+SNAP_ID = AwsEC2Tools().create_snapshot([INSTANCE_ID, ])
+print SNAP_ID
+time.sleep(10)
+print AwsEC2Tools().register_image(SNAP_ID[0].id, '/dev/sda1', True)
