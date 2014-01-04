85640c2c2560079a0e00cf3a0ec43e3332b3da37
Toomore Chiang <toomore0929@gmail.com>
2014-01-04 15:55:58 +0000

    awss3tools tiny rewrite and try save data.

--- awss3tools.py
+++ awss3tools.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
+from cStringIO import StringIO
 import setting
 
 
 class AwsS3Tools(object):
-    def __init__(self):
+    def __init__(self, bucket):
         self.conn = S3Connection(setting.ID, setting.KEY)
+        self.bucket = self.conn.get_bucket(bucket)
 
-    def get_bucket(self, bucket):
-        return self.conn.get_bucket(bucket)
+    def open(self, filename):
+        get_files = self.bucket.get_key(filename)
+
+        if get_files:
+            return get_files
+
+        return self.bucket.new_key(filename)
 
 if __name__ == '__main__':
-    print AwsS3Tools().get_bucket('toomore-aet').get_all_keys()
+    bucket = AwsS3Tools('toomore-aet').bucket
+    print bucket.get_all_keys()
+    print dir(bucket)
+
+    files = AwsS3Tools('toomore-aet').open('toomore.txt')
+    print files
+    print dir(files)
+
+    # ----- save data ----- #
+    content = StringIO()
+    content.write('Toomore is 蔣太多')
+    print files.set_contents_from_string(content.getvalue())
