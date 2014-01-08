f6d16c5aaaebb2e17cd8f834b6b67684becf948a
Toomore Chiang <toomore0929@gmail.com>
2014-01-08 16:19:16 +0000

    Add `save` string/file.

--- awss3tools.py
+++ awss3tools.py
@@ -6,9 +6,10 @@ import setting
 
 
 class AwsS3Tools(object):
-    def __init__(self, bucket):
+    def __init__(self, bucket, open_file=None):
         self.conn = S3Connection(setting.ID, setting.KEY)
         self.bucket = self.conn.get_bucket(bucket)
+        self.keys = self.open(open_file)
 
     def open(self, filename):
         get_files = self.bucket.get_key(filename)
@@ -18,36 +19,35 @@ class AwsS3Tools(object):
 
         return self.bucket.new_key(filename)
 
-    @staticmethod
-    def set_contents(key, file_data, make_public=None):
+    def save(self, file_data, make_public=None):
         if isinstance(file_data, file):
-            result = key.set_contents_from_file(file_data)
+            result = self.keys.set_contents_from_file(file_data)
         elif isinstance(file_data, str):
-            result = key.set_contents_from_string(file_data)
+            result = self.keys.set_contents_from_string(file_data)
         else:
             return type(file_data)
 
-        key.make_public() if make_public else None
+        self.keys.make_public() if make_public else None
         return result
 
 if __name__ == '__main__':
-    bucket = AwsS3Tools('toomore-aet').bucket
-    print bucket.get_all_keys()
-    print dir(bucket)
+    #bucket = AwsS3Tools('toomore-aet').bucket
+    #print bucket.get_all_keys()
+    #print dir(bucket)
 
     # ----- create data ----- #
-    files = AwsS3Tools('toomore-aet').open('toomore.txt')
+    files = AwsS3Tools('toomore-aet', 'toomore.txt')
     print files
     print dir(files)
 
     # ----- save data ----- #
-    #content = StringIO()
-    #content.write('Toomore is 蔣太多')
-    #print AwsS3Tools.set_contents(files, content.getvalue(), True)
+    content = StringIO()
+    content.write('Toomore is 蔣太多')
+    print files.save(content.getvalue(), True)
 
     # ----- save files ----- #
-    #with open('./README.md') as file_data:
-    #    print AwsS3Tools.set_contents(files, file_data)
+    with open('./README.md') as file_data:
+        print files.save(file_data, True)
 
     # ----- read files ----- #
     #content = StringIO()
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     #print content.getvalue()
 
     # ----- delete files ----- #
-    print files.delete()
+    #print files.delete()
 
     # ----- generate url ----- #
     #print files.generate_url(30)
