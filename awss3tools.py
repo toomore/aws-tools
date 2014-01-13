49d22fb4af920a979761be04e8c69294169d45c5
Toomore Chiang <toomore0929@gmail.com>
2014-01-13 18:41:26 +0000

    PEP8 ./awss3tools.py

--- awss3tools.py
+++ awss3tools.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
+''' AwsS3Tools  '''
 import setting
 from boto.s3.connection import S3Connection
-from boto.s3.key import Key
 from cStringIO import InputType
 from cStringIO import OutputType
 from cStringIO import StringIO
 
 
 class AwsS3Tools(object):
+    ''' AwsS3Tools '''
     def __init__(self, bucket, open_file=None):
         self.conn = S3Connection(setting.ID, setting.KEY)
         self.bucket = self.conn.get_bucket(bucket)
         self.keys = self.open(open_file)
 
     def open(self, filename):
+        ''' Open a file by keyname '''
         get_files = self.bucket.get_key(filename)
 
         if get_files:
@@ -22,6 +24,7 @@ class AwsS3Tools(object):
         return self.bucket.new_key(filename)
 
     def save(self, file_data, make_public=None):
+        ''' Save file to S3 '''
         if isinstance(file_data, (file, InputType, OutputType)):
             if isinstance(file_data, (InputType, OutputType)):
                 file_data.seek(0)
@@ -32,10 +35,13 @@ class AwsS3Tools(object):
         else:
             return type(file_data)
 
-        self.keys.make_public() if make_public else None
+        if make_public:
+            self.keys.make_public()
+
         return result
 
     def read(self):
+        ''' Read file from S3 '''
         result = StringIO()
         self.keys.get_contents_to_file(result)
         result.seek(0)
@@ -43,9 +49,11 @@ class AwsS3Tools(object):
         return result
 
     def update(self, file_data, *args, **kwargs):
+        ''' Update file. '''
         return self.save(file_data, *args, **kwargs)
 
     def delete(self):
+        ''' Delete file. '''
         return self.keys.delete()
 
 if __name__ == '__main__':
@@ -54,36 +62,36 @@ if __name__ == '__main__':
     #print dir(bucket)
 
     # ----- create data ----- #
-    files = AwsS3Tools('toomore-aet', 'toomore.txt')
-    print files
-    print dir(files)
+    FILES = AwsS3Tools('toomore-aet', 'toomore.txt')
+    print FILES
+    print dir(FILES)
 
     # ----- save data ----- #
     #content = StringIO()
     #content.write('Toomore is 蔣太多')
-    #print files.save(content.getvalue(), True)
+    #print FILES.save(content.getvalue(), True)
 
     # ----- save files ----- #
     #with open('./README.md') as file_data:
-    #    print files.save(file_data, True)
+    #    print FILES.save(file_data, True)
 
     # ----- read files ----- #
-    #content = files.read()
+    #content = FILES.read()
     #print content.read()
 
     # ----- update files ----- #
     #content.writelines('Toomore is Toomore')
     #content.writelines('Toomore is Toomore')
-    #print files.update(content)
-    #print files.read().getvalue()
+    #print FILES.update(content)
+    #print FILES.read().getvalue()
 
     # ----- get keys acl ----- #
-    #print dir(files.keys)
-    #print dir(files.keys.get_acl())
-    #print files.keys.get_acl().to_xml()
+    #print dir(FILES.keys)
+    #print dir(FILES.keys.get_acl())
+    #print FILES.keys.get_acl().to_xml()
 
     # ----- delete files ----- #
-    #print files.delete()
+    #print FILES.delete()
 
     # ----- generate url ----- #
-    #print files.generate_url(30)
+    #print FILES.generate_url(30)
