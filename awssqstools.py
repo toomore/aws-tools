0350efa247805d3d4a1945f44d026eebc910f458
Toomore Chiang <toomore0929@gmail.com>
2014-01-17 01:43:32 +0800

    Add doc into awssqstools.

--- awssqstools.py
+++ awssqstools.py
@@ -5,7 +5,10 @@ from boto.sqs import connect_to_region
 
 
 class AwsSQSTools(object):
-    ''' AwsSQSTools '''
+    ''' AwsSQSTools
+
+        :param str queue_name: AWS SQS queue name.
+    '''
     def __init__(self, queue_name):
         self.conn = connect_to_region(setting.REGION,
                                       aws_access_key_id=setting.ID,
@@ -17,13 +20,25 @@ class AwsSQSTools(object):
         return self.conn.get_all_queues(*args, **kwargs)
 
     def write(self, body):
-        ''' Write a message into queue '''
+        ''' Write a message into queue
+
+            :param str body: message body
+            :rtype: :class:`boto.sqs.message.Message`
+            :returns: :class:`boto.sqs.message.Message`
+        '''
 
         return self.queue.write(self.queue.new_message(body))
 
     def get_messages(self, num_messages=10, *args, **kwargs):
-        ''' Get messages from queue '''
-        #TODO Need to increase `get_messages` concurrency.
+        ''' Get messages from queue
+
+            :param int num_messages: get message in one time.
+            :rtype: list
+            :returns: a list of message body.
+
+            .. todo:: Need to increase `get_messages` concurrency.
+
+        '''
         for i in self.queue.get_messages(num_messages, *args, **kwargs):
             yield i.get_body()
 
