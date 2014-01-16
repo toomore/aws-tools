a3727b9deafaaefc5e6243b416b9a368a4a009f3
Toomore Chiang <toomore0929@gmail.com>
2014-01-17 00:05:14 +0800

    Changed in awssestools.

--- awssestools.py
+++ awssestools.py
@@ -15,18 +15,24 @@ class AwsSESTools(object):
 
     @staticmethod
     def mail_header(name, mail):
-        ''' Encode header to base64 '''
+        ''' Encode header to base64
+
+            :param str name: user name
+            :param str mail: user mail
+            :rtype: string
+            :returns: a string of "name <mail>" in base64.
+        '''
         return '"%s" <%s>' % (Header(name, 'utf-8'), mail)
 
-    def send_email(self):
+    def send_email(self, *args, **kwargs):
         ''' Send email '''
-        return self.conn.send_email(
-                source=self.mail_header(u'蔣偉志', 'me@toomore.net'),
-                to_addresses=self.mail_header(u'蔣太多', 'toomore0929@gmail.com'),
-                subject=u'測試寄件者中文問題',
-                body=u'測試寄件者中文問題',
-                format='html')
+        return self.conn.send_email(*args, **kwargs)
 
 if __name__ == '__main__':
     print 'remove comment before use'
-    #print AwsSESTools().send_email()
+    #print AwsSESTools().send_email(
+    #        source=self.mail_header(u'蔣偉志', 'me@toomore.net'),
+    #        to_addresses=self.mail_header(u'蔣太多', 'toomore0929@gmail.com'),
+    #        subject=u'測試寄件者中文問題',
+    #        body=u'測試寄件者中文問題',
+    #        format='html')
