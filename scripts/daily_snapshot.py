4f857a0b647c8d39d3913ecc55af206db77626b6
Toomore Chiang <toomore0929@gmail.com>
2014-01-25 02:43:57 +0000

    Using while for waitting create snapshot to register AMI.

--- scripts/daily_snapshot.py
+++ scripts/daily_snapshot.py
@@ -5,11 +5,24 @@ sys.path.insert(0, '../')
 import time
 from awsec2tools import AwsEC2MetaData
 from awsec2tools import AwsEC2Tools
+from boto.exception import EC2ResponseError
+from datetime import datetime
 
 
 # Deploy to an instance and using cron.
 INSTANCE_ID = AwsEC2MetaData().get('instance-id')
 SNAP_ID = AwsEC2Tools().create_snapshot([INSTANCE_ID, ])
 print SNAP_ID
-time.sleep(10)
-print AwsEC2Tools().register_image(SNAP_ID[0].id, '/dev/sda1', True)
+#time.sleep(10)
+#print AwsEC2Tools().register_image(SNAP_ID[0].id, '/dev/sda1', True)
+
+# Register image
+AMIOK_LOOP = True
+while AMIOK_LOOP:
+    try:
+        print AwsEC2Tools().register_image(SNAP_ID[0].id, '/dev/sda1', True)
+        AMIOK_LOOP = False
+    except EC2ResponseError:
+        print '%s: %s is not completed' % (datetime.now(), SNAP_ID[0].id)
+        print 'Waitting 10 seconds ...'
+        time.sleep(10)
