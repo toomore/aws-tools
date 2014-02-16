c2bd196f28897616703399de929030346bf4f56b
Toomore Chiang <toomore0929@gmail.com>
2014-02-16 21:39:56 +0800

    Move setting from class.

--- awssestools.py
+++ awssestools.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 ''' My AWS Tools '''
-import setting
 from boto import ses
 from email.header import Header
 
 
 class AwsSESTools(object):
     ''' AWS SES tools '''
-    def __init__(self):
-        ''' Make a connect '''
+    def __init__(self, aws_access_key_id, aws_secret_access_key):
+        ''' Make a connect
+
+            :param aws_access_key_id str: aws_access_key_id
+            :param aws_secret_access_key str: aws_secret_access_key
+        '''
         self.conn = ses.connection.SESConnection(
-                                     aws_access_key_id=setting.ID,
-                                     aws_secret_access_key=setting.KEY)
+                                    aws_access_key_id=aws_access_key_id,
+                                    aws_secret_access_key=aws_secret_access_key)
 
     @staticmethod
     def mail_header(name, mail):
@@ -33,9 +36,11 @@ class AwsSESTools(object):
 
 if __name__ == '__main__':
     print 'remove comment before use'
-    #print AwsSESTools().send_email(
-    #        source=self.mail_header(u'蔣偉志', 'me@toomore.net'),
-    #        to_addresses=self.mail_header(u'蔣太多', 'toomore0929@gmail.com'),
+    #import setting
+    #print AwsSESTools(setting.ID, setting.KEY).send_email(
+    #        source=AwsSESTools.mail_header(u'蔣偉志', 'me@toomore.net'),
+    #        to_addresses=AwsSESTools.mail_header(u'蔣太多',
+    #                'toomore0929@gmail.com'),
     #        subject=u'測試寄件者中文問題',
     #        body=u'測試寄件者中文問題',
     #        format='html')
