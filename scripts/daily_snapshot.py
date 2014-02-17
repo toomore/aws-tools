f4c9e802d773817f9b698cd0046fc4f2dde17189
Toomore Chiang <toomore0929@gmail.com>
2014-02-17 19:22:21 +0000

    Tiny changed in scripts.

--- scripts/daily_snapshot.py
+++ scripts/daily_snapshot.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.insert(0, '../')
 
+import setting
 import time
 from awsec2tools import AwsEC2MetaData
 from awsec2tools import AwsEC2Tools
@@ -11,15 +12,16 @@ from datetime import datetime
 
 # Deploy to an instance and using cron.
 INSTANCE_ID = AwsEC2MetaData().get('instance-id')
-SNAP_ID = AwsEC2Tools().create_snapshot([INSTANCE_ID, ])
+EC2 = AwsEC2Tools(setting.REGION, setting.ID, setting.KEY)
+SNAP_ID = EC2.create_snapshot([INSTANCE_ID, ])
 print SNAP_ID
 #time.sleep(10)
-#print AwsEC2Tools().register_image(SNAP_ID[0].id, '/dev/sda1', True)
+#print EC2.register_image(SNAP_ID[0].id, '/dev/sda1', True)
 
 # Register image
 while True:
     try:
-        print AwsEC2Tools().register_image(SNAP_ID[0].id, '/dev/sda1', True)
+        print EC2.register_image(SNAP_ID[0].id, '/dev/sda1', True)
         break
     except EC2ResponseError:
         print '%s: %s is not completed' % (datetime.now(), SNAP_ID[0].id)
