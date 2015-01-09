bb6d5043d8ea84022a5facb890a94894a4dea193
Toomore Chiang <toomore0929@gmail.com>
2015-01-09 09:23:34 +0000

    Add `render_ics`.

--- utils.py
+++ utils.py
@@ -25,26 +25,56 @@ DTEND;TZID=Asia/Taipei:%(end)s
 STATUS:CONFIRMED
 SUMMARY:%(title)s
 DESCRIPTION:%(description)s
-ORGANIZER;CN=Meetup Reminder:MAILTO:%(admin_mail)s
+ORGANIZER;CN=%(admin)s Reminder:MAILTO:%(admin_mail)s
 CLASS:PUBLIC
 CREATED:%(created)s
 LOCATION:%(location)s
-URL:http://www.meetup.com/Taipei-py/events/219688456/
+URL:%(url)s
 LAST-MODIFIED:%(created)s
 UID:%(admin_mail)s
 END:VEVENT
 END:VCALENDAR
 '''
+# created, start, end, title, description, location, admin_mail
 # date format: 20150108T073253Z
 # DTSTART: 20150113T190000
 # DTEND: 20150113T220000
 # GEO:25.02;121.44
 
-def dateisoformat(date=None):
+def dateisoformat(date=None, with_z=True):
     if not date:
         date = datetime.utcnow() + timedelta(hours=8)
 
-    return date.strftime('%Y%m%dT%H%M%SZ')
+    if with_z:
+        return date.strftime('%Y%m%dT%H%M%SZ')
+    return date.strftime('%Y%m%dT%H%M%SZ')[:-1]
+
+
+def render_ics(title, description, location, start, end, created, admin,
+        admin_mail, url):
+    data = {
+            'title': title,
+            'description': description,
+            'location': location,
+            'start': dateisoformat(start, False),
+            'end': dateisoformat(end, False),
+            'created': dateisoformat(created),
+            'admin': admin,
+            'admin_mail': admin_mail,
+            'url': url,
+            }
+    return BASEICS % data
 
 if __name__ == '__main__':
     print dateisoformat()
+    print render_ics(
+            title=u'吃火鍋',
+            description=u'冬天到了要吃火鍋！',
+            location=u'台北市中正區衡陽路51號',
+            start=datetime(2015, 1, 29, 10),
+            end=datetime(2015, 1, 29, 18, 30),
+            created=None,
+            admin=u'Toomore',
+            admin_mail=u'toomore0929@gmail.com',
+            url=u'http://toomore.net/'
+            )
